3a9b3bbcdca49e74e0b7714dc7f519c945523201
iXaoh <sebastienhals@hotmail.com>
2016-12-15 11:05:41 +0100

    Update server.c

--- Projet C/Server/server.c
+++ Projet C/Server/server.c
@@ -1,1 +1,17 @@
-#include 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+
+
+int shmid
+int main(){
+	char c;
+	int chmid;
+	key_t key;
+	char *shm, *s;
+	
+	key = 5678;
+}
